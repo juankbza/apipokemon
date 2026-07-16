dfa980c1d3d3110da1ee6487b8c12c159c28cf5c
Anyer David Villarroel Ramos <avillarroel.9139@unimar.edu.ve>
2026-07-16 17:50:06 -0400

    bienvenida del doctor oak

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,19 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:pokemania/screens/screens.dart';
 
 class AppRoutes {
-
-  static String initialRoute = "/home";
+  static String initialRoute = "/info";
 
   static Map<String, Widget Function(BuildContext)> routes = {
-
-    '/info' : (BuildContext context) => InfoScreen(),
-    '/home' : (BuildContext context) => HomeScreen(),
-    '/guia' : (BuildContext context) => GuiaScreen(),
-    
-
-
+    '/info': (BuildContext context) => InfoScreen(),
+    '/home': (BuildContext context) => HomeScreen(),
+    '/guia': (BuildContext context) => GuiaScreen(),
   };
-
-
-
 }
